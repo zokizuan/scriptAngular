0328926391c577b3648b342e4ef2caed19ade966
Ahmed Hamza <81289670+ham-zax@users.noreply.github.com>
2022-03-04 23:47:49 +0530

    made css improvement

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,5 +1,5 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 
 
@@ -8,14 +8,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent implements OnInit,AfterViewInit {
 
   // function to generate coins
   CoinArrayGenerator = (coinsConfig: object) => {
     return Object.entries(coinsConfig).map(([k, v]) => {
       let arr: string[] = []
       for (let i = 1; i < (v + 1); i++) {
-        arr.push(k + "_coin-" + i)
+        arr.push("coin-" + i)
       }
       return arr
     }).flat()
@@ -25,8 +25,8 @@ export class BoardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    
   }
+ 
   coinsConfig = {
     yellow: 10,
     red: 10,
@@ -40,31 +40,43 @@ export class BoardComponent implements OnInit {
   BlueArr = [] as any[];
   RedArr = [] as any[];
   TotalBoard = [this.GreenArr, this.RedArr, this.BlueArr, this.YellowArr];
-  /* get isTodoAvailable(): boolean {
-    return this.todo && this.todo.length < 5;
-  } */
+  
+  
+  @ViewChild('test')
+  test!: ElementRef;
   get isDoneAvailable(): boolean {
-    return this.GreenArr && this.GreenArr.length < 40;
+    return this.GreenArr && this.GreenArr.length < 10;
   }
-  /* todoPredicate = (): boolean => {
-    return this.isTodoAvailable;
-  } */
-  donePredicate = (): boolean => {
+  ngAfterViewInit(): void {
+    const divEl: HTMLDivElement = this.test.nativeElement;
+    console.log(divEl);
+  }
+  arrLengthCheck!: boolean;
+  
+  checkArrayLength(arr:any[]):boolean {
+    return arr && arr.length < 10;  
+    // return true
+  }
+  
+  checkArrayLengthPredicate = (): boolean => {
+    console.log(this.isDoneAvailable,)
     return this.isDoneAvailable;
   }
-  // items=['1','2','3','4','5','6','7','8','9','10',]
-  items = [undefined];
-  positions!:any[];
-  test() {
-    console.log(this.YellowArr, this.GreenArr, this.BlueArr, this.RedArr);
-  }
+  
+  
+  
   drop(event: CdkDragDrop<any>) {
+    if (event.container.data.length >= 10) { 
+      console.log("dont drop here")
+      return
+      // console.log(arrivalList.length)
+    }
     console.log(event);
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
       transferArrayItem(
-        event.previousContainer.data,
+        event.previousContainer.data, 
         event.container.data,
         event.previousIndex,
         event.currentIndex,
